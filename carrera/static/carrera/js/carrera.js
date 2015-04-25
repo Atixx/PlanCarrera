41d5d2ff4cf1bc0f33a26a71a44f7c75693b7480
Atixx <levym10@gmail.com>
2015-04-25 12:38:54 -0300

    nueva base para trabajar sobre modal

--- carrera/static/carrera/js/carrera.js
+++ carrera/static/carrera/js/carrera.js
@@ -30,5 +30,17 @@ $(document).ready( function()
 
 
   
+    $("#esteboton").click(function(ev) // for each edit materia url
+  { 
+    ev.preventDefault(); // prevent navigation
+    var url = $(this).data("form"); // get the contact form url
+    $("#muestra").load(url, function()  // load the url into the modal
+    {
+        $(this).modal('show'); // display the modal on url load
+    });
+    return false; // prevent the click propagation
+    
+    });
+  
   
 }); 
