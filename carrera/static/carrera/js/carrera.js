80373a83ffd57e753ccc5859286d4a9c11640718
Maor <levym10@gmail.com>
2015-05-08 23:27:57 -0300

    arreglado listar materias para que muestre los 5 anios

--- carrera/static/carrera/js/carrera.js
+++ carrera/static/carrera/js/carrera.js
@@ -14,29 +14,8 @@ $(document).ready( function()
   {
     $("#contenido_examen").slideToggle();
   });
-  /*
-  $(".materia").click(function()
-  {
-    $(this).fadeToggle("slow",function()
-    {
-        $(this).fadeToggle("slow");
-    });
-  });*/
   
   $(".list-group-item").click(function(ev) // for each edit materia url
-  { 
-    ev.preventDefault(); // prevent navigation
-    var url = $(this).data("form"); // get the contact form url
-    $("#muestra").load(url, function()  // load the url into the modal
-    {
-        $(this).modal('show'); // display the modal on url load
-    });
-    return false; // prevent the click propagation
-    
-    });
-
-  
-    $(".muestraModal").click(function(ev) // for each edit materia url
   { 
     ev.preventDefault(); // prevent navigation
     var url = $(this).data("form"); // get the contact form url
