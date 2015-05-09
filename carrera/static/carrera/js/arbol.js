ffc47cced3d625b629eb327c763e8509504c9b5f
Maor <levym10@gmail.com>
2015-05-09 12:05:42 -0300

    base para cargar todas las materias y sus estados en JS

--- carrera/static/carrera/js/arbol.js
+++ carrera/static/carrera/js/arbol.js
@@ -1,11 +1,26 @@
 $(document).ready( function()
 {
 
+    function materia(nombre, estado)
+    {
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    //TODO cargar todas las materias en un array de materias
+
+
     $('.list-group-item').click(function () {
       var classes = ['list-group-item list-group-item-warning active','list-group-item list-group-item-danger active', 'list-group-item list-group-item-success active', 'list-group-item list-group-item active'];
       $(this).each(function(){
         this.className = classes[($.inArray(this.className, classes)+1)%classes.length];
       });
+    });
+    
+    
+    $('#alert').click(function ()
+    {
+        alert(estados[0].fields.estado);
     });
 
 
