ff3da095835a1256f7a267f69db75867f1655de9
sebastian-bascu <sebastian.bascu@gmail.com>
2015-05-10 14:08:55 -0300

    Update RecuperarCredenciales.py

--- funciones/RecuperarCredenciales.py
+++ funciones/RecuperarCredenciales.py
@@ -14,7 +14,7 @@ def RecuperarPassword(correo):
     u=User.objects.get(email=correo)
     createidcode="UserID:"+ str(u.id)
     idcode="AA="+base64.standard_b64encode(createidcode)
-    url= "http://localhost:8000/plan/recuperar?"+idcode
+    url= "http://atix.koding.io:80/plan/recuperar?"+idcode
     send_mail('Plan Carrera: Recuperar password', 'Para poder continuar y recuperar su clave, siga el siguiente link: '
               +url, 'gestion.plancarrera@gmail.com', [correo], fail_silently=False)
     
@@ -34,7 +34,7 @@ def RecuperarUsuario(correo):
     usuario=u.username
     createidcode="UserID:"+ str(u.id)
     idcode="AA="+base64.standard_b64encode(createidcode)
-    url= "http://localhost:8000/plan/recuperar?"+idcode
+    url= "http://atix.koding.io:80/plan/recuperar?"+idcode
     send_mail('Plan Carrera: Recuperar Usuario', 'Su usuario es: '
               +usuario +'. Si no recuerda su clave de acceso puede cambiarla siguiendo este link: '
               +url, 'gestion.plancarrera@gmail.com', [correo], fail_silently=False)
